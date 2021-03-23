10ec1d2c30b23b2aed81f63e2fd4f47e4138e81f
Higashi <hanzooferly@gmail.com>
2021-03-23 18:50:22 +0300

    Commit message

---
+++ src/main/java/ru/geekbrains/lesson8/Wall.java
@@ -0,0 +1,15 @@
+package ru.geekbrains.lesson8;
+
+public class Wall extends Obstacle{
+
+    private int height;
+
+    public Wall(int height) {
+        this.height = height;
+    }
+
+    public int getHeight() {
+        return height;
+    }
+
+}
